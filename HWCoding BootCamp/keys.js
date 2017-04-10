78e84a6a5bd7b2aabfce58363cc0efb12125127b
Nathalie Robin <Nathalie Robin>
2017-04-10 18:24:05 -0400

    test

---
+++ HWCoding BootCamp/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+ 
+ exports.twitterKeys = {
+   consumer_key: '99Gh1QrNNwkspR7iRge0hSQbW',
+   consumer_secret: 'AiQW4mbr7DsBEMtaNGdcaaTbY5TEIgA7KnZaIejWXrJlQxM9Ri',
+   access_token_key: '82421155-YlvQRK3k7FizF3kRpiJ7NmD4qkkmbRoqWNNKwBMnW',
+   access_token_secret: 'S7GxWpmCpJzKwh7j9A0BXuKIYlDeyxzYxLLymQ3fulhe2',
+ }
